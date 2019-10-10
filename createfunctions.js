f4584d3cd0ed77a0f35ecbb27ce3ee0a261f9f50
EviIoannou <evievi@outlook.com>
2019-10-10 11:49:15 +0200

    adding flip and compare functions

--- createfunctions.js
+++ createfunctions.js
@@ -1,11 +1,17 @@
-var cards= [1,3,5,7,1,5,6];
-function compareCards(card1, card2) {
-  if (card1 == card2){
+//player should only be able to click on two cards
+function compareCards() {
+  var card1 = document.querySelector();
+  var card2 = document.querySelector();
+  if (card1.id == card2.id){
+    //delete both cards
   }
   else {
-    
+    //flip cards back and player clicks two new cards
   }
 }
 
 
-//myCard.addEventListener("click", compareCards);
+//myCard.addEventListener("click", flipCard);
+function flipCard () {
+  
+}
